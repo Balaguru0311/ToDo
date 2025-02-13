db0b8ef5e5cfe04b31eb23de292051d0e2f07362
Balaguru0311 <balasubiramanigurusamy@gmail.com>
2025-02-13 18:05:57 +0530

    Base changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Todo",
+  base:"/ToDo",
   plugins: [react()],
   test: {  
     globals: true, // Allow the use of global variables like describe, it, etc.  
